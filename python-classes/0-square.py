e9e157d3e1888cdd6bfdd9ace13f0042e264149d
brownstracey <p.browns@alustudent.com>
2024-06-30 13:01:53 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-'''
-empty class
-'''
+"""
+0-square.py
+
+This module provides an empty class Square that defines a square.
+"""
+
 
 class Square:
+    """
+    An empty class that defines a square.
+
+    This class does not have any attributes or methods.
+    """
     pass
