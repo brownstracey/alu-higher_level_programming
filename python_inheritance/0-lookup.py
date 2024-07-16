ad02d170c3b730414740f6cad9a834f4b2a836a1
brownstracey <p.browns@alustudent.com>
2024-07-15 23:12:09 -0700

    completed tasks

--- python_inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-'''A function that returns the available atts and methods of an object '''
-
-
-def lookup(obj):
-    '''Take an object and return its atts and methods'''
-    return list(dir(obj))
