01ff3af3effca3e3f45d6c701af2c5f126fb676b
brownstracey <p.browns@alustudent.com>
2024-08-02 03:11:18 -0700

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@
 
 import requests
 if __name__ == "__main__":
-    requ = requests.get('https://intranet.hbtn.io/status')
+    requ = requests.get('https://alu-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(requ.text)))
     print("\t- content: {}".format(requ.text))
